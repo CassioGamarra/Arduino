30f354d31bcf459e9314858cd165ae10aa26c844
cassiogamarra <cassiogamarra@outlook>
2021-10-14 01:09:10 -0300

    Atualizado para utilizar apenas o bluetooth

--- leitorRFIDBovino/leitorRFIDBovino.ino
+++ leitorRFIDBovino/leitorRFIDBovino.ino
@@ -1,8 +1,7 @@
 #include <SoftwareSerial.h> //Biblioteca SoftwareSerial
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
-#include <DS1307.h> //Biblioteca RTC DS1307
-#include <SD.h> //Biblioteca do cartão SD 
+#include <LiquidCrystal_I2C.h> 
+#include <DS1307.h> //Biblioteca RTC DS1307 
 
 // Inicializa o display no endereco 0x27
 LiquidCrystal_I2C lcd(0x27, 16, 3);
@@ -14,57 +13,9 @@ DS1307 rtc(A2, A3);
 int chipSelect = 4; //Pino 4 para o adaptador MicroSD
 
 //o código gravado no brinco é em char, então não existe necessidade de usar inteiro.
-char c;
-
+char c; 
 String nome = "";
-
-int bip = 8;
-int QTDLinhas = 0;
-
-
-//Função para contar as linhas no arquivo 
-int ContarLinhas(){
-  File Arquivo = SD.open("brincos.txt"); //Arquivo contendo os dados de alerta, no formato: númeroBrinco : alerta
-  if(Arquivo){
-    while(Arquivo.available()){
-        byte B = Arquivo.read();
-        if(B == '\n'){
-          QTDLinhas++;   
-        }
-    }      
-    Arquivo.close();  
-  }
-  return QTDLinhas;
-}
-
-//Função para buscar as linhas no arquivo.
-void BuscarLinhas(int QTDLinhas, String* Linhas){
-  int cont = 0;
-  File Arquivo = SD.open("brincos.txt"); //Arquivo contendo os dados de alerta, no formato: númeroBrinco : alerta
-  if(Arquivo){
-    while(Arquivo.available()){
-      char crtr = (char)Arquivo.read();
-      if(crtr == '\n'){
-        cont++;
-      }else Linhas[cont] += crtr;
-    }
-    Arquivo.close();
-  }
-  return Linhas;
-}
-
-
-//Função para verificar a presença de um brinco com status ativo no arquivo
-bool TestarPresenca(String Entrada, String *Status, String Linhas[]){
-  int cont = 0;
-  for(cont = 0; cont < QTDLinhas; cont++){
-    if(Entrada.substring(0,16) == Linhas[cont].substring(0,16)){
-      *Status = Linhas[cont].substring(17);
-      return true;
-    }
-  }
-return false;
-}
+int bip = 8; 
 
 void setup(){
   Serial.begin(9600);
@@ -76,16 +27,12 @@ void setup(){
   RFID.begin(9600); // Seta o DataRate do leitor de RFID
 
   pinMode (bip, OUTPUT);
-
-  pinMode(chipSelect, OUTPUT);// O MicroSD no pino 4 precisa estar como OUTPUT
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Não foi possível iniciar o cartão SD"); //Caso de o cartão não esteja disponível
-  }
+                                          
   rtc.halt(true);
   //As linhas abaixo setam a data e hora do modulo, após a primeira utilização podem ser comentadas.
-  rtc.setDOW(WEDNESDAY);      //Define o dia da semana
-  rtc.setTime(21, 30, 0);     //Define o horario
-  rtc.setDate(07, 11, 2018);   //Define o dia, mes e ano
+  rtc.setDOW(THURSDAY);      //Define o dia da semana
+  rtc.setTime(00, 16, 0);     //Define  o horario
+  rtc.setDate(14, 10, 2021);   //Define o dia, mes e ano
 
   //Definicoes do pino SQW/Out
   rtc.setSQWRate(SQW_RATE_1);
@@ -102,45 +49,18 @@ void loop()
     delay(100);
     lcd.clear();
     lcd.print("NUMERO DO BRINCO");
-    lcd.setCursor(0, 1);
+     lcd.setCursor(0, 1);
     while (RFID.available() > 0) { //Enquanto o leitor estiver disponível, realiza a leitura
       c = RFID.read(); // C recebe os dados do Leitor
       Serial.write(c);
       nome = String(nome + c); //
       if (c == '\r')  Serial.write('\n');
-    }
+    } 
+    lcd.print(nome);  
     digitalWrite(bip, HIGH);
     delay(300);
     digitalWrite(bip, LOW);
-    delay(100);
-    QTDLinhas = ContarLinhas() + 1;
-    String Linhas[QTDLinhas];
-    BuscarLinhas(QTDLinhas,Linhas);
-    String Status;
-    if(TestarPresenca(nome, &Status, Linhas)){
-      Status.toUpperCase();
-      Serial.println(Status);
-      lcd.clear();
-      lcd.print(nome);
-      lcd.setCursor(0, 1);
-      lcd.print(Status);
-      delay(100);
-      digitalWrite(bip, HIGH);
-      delay(300);
-      digitalWrite(bip, LOW);
-      delay(300);
-      digitalWrite(bip, HIGH);
-      delay(300);
-      digitalWrite(bip, LOW);  
-    }else 
-    {
-      Serial.println("SEM ALERTA");
-      lcd.print(nome);    
-    }
-    nome = String(nome + " DATA " + rtc.getDateStr());
-    File arquivo = SD.open("arquivo.txt", FILE_WRITE);
-    arquivo.println(nome);
-    arquivo.close();
+    delay(100); 
     nome = "";
   }
 }
